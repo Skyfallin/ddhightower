9d0bc588ec8367ed8a82963be2fcfe16884f14b0
Dimetrius Hightower <dimetrius.hightower@gmail.com>
2024-06-05 20:28:57 -0400

    reduce margin

--- app/more-stories.tsx
+++ app/more-stories.tsx
@@ -1,5 +1,12 @@
-import { Box, Card, CardBody, Heading, Link, Text } from "@chakra-ui/react";
-import Avatar from "./avatar";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  Heading,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import TopShadow from "./components/top-shadow";
 import CoverImage from "./cover-image";
 import DateComponent from "./date";
@@ -22,6 +29,7 @@ function PostPreview({
   return (
     <div className="lg-min-w-740">
       <Card mx="auto" my={5} shadow={"lg"} boxShadow={"lg"}>
+        <Button w="auto">Test</Button>
         <TopShadow />
         <Box display={"flex"} flexDirection={"column"}>
           <Box alignSelf={"center"} paddingTop={"16px"}>
@@ -39,14 +47,7 @@ function PostPreview({
             <Box mb={4}>
               <DateComponent dateString={date} />
             </Box>
-            <Text fontSize="lg" mb={4}>
-              {excerpt}
-            </Text>
-            {author && (
-              <Box display="flex" alignItems="center">
-                <Avatar name={author.name} picture={author.picture} />
-              </Box>
-            )}
+            <Text fontSize="lg">{excerpt}</Text>
           </CardBody>
         </Box>
       </Card>
@@ -60,7 +61,7 @@ export default function MoreStories({ morePosts }: { morePosts: any[] }) {
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
         More Stories
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-4 mb-32">
         {morePosts.map((post) => (
           <PostPreview
             key={post.slug}
