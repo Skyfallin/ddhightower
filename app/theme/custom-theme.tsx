1d81800c38a9dd57da9117f0ac225c0df76836fd
Dimetrius Hightower <dimetrius.hightower@gmail.com>
2024-06-06 20:18:09 -0400

    more custom theme styles

--- app/theme/custom-theme.tsx
+++ app/theme/custom-theme.tsx
@@ -2,16 +2,33 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const customTheme = extendTheme({
+  components: {
+    Box: {
+      baseStyle: {
+        backgroundColor: "#faebd7", // Antique White for Box component (used as card)
+      },
+    },
+    Card: {
+      baseStyle: {
+        container: {
+          backgroundColor: "#faebd7", // Antique White
+        },
+      },
+    },
+    Text: {
+      fontSize: "sm", // Set default body font size to 'sm'
+    },
+  },
   styles: {
     global: {
-      "html, body": {
+      body: {
         backgroundColor: "#faebd7", // Antique white
+        fontSize: "sm", // Set default body font size to 'sm'
       },
     },
   },
   fonts: {
     heading: "'Merriweather', serif",
-    // body: "'Roboto Slab', serif",
     body: "'Libre Baskerville', serif",
   },
 });
