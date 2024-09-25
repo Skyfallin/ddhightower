058634021d1064b4bf0530412b0c15a18fbf7296
Dimetrius Hightower <dimetrius.hightower@gmail.com>
2024-09-25 18:47:43 -0400

    adding navbar icons

--- app/components/header-bar.tsx
+++ app/components/header-bar.tsx
@@ -1,35 +1,51 @@
 import React from "react";
-import { FaTwitter } from "react-icons/fa";
+import {
+  FaDiscord,
+  FaInstagram,
+  FaTiktok,
+  FaXTwitter,
+  FaYoutube,
+} from "react-icons/fa6";
 
 interface SocialLink {
   icon: JSX.Element;
   hyperlink: string;
 }
 
-interface HeaderBarProps {
-  socialLinks: SocialLink[];
-}
-
 const socialLinks: SocialLink[] = [
   {
-    icon: (
-      <FaTwitter color="#7A8450" style={{ height: "100%", width: "auto" }} />
-    ),
-    hyperlink: "text",
+    icon: <FaDiscord size={"24"} />,
+    hyperlink: "https://x.com/",
+  },
+  {
+    icon: <FaInstagram size={"24"} />,
+    hyperlink: "https://x.com/",
+  },
+  {
+    icon: <FaTiktok size={"24"} />,
+    hyperlink: "https://x.com/",
+  },
+  {
+    icon: <FaXTwitter size={"24"} />,
+    hyperlink: "https://x.com/",
+  },
+  {
+    icon: <FaYoutube size={"24"} />,
+    hyperlink: "https://x.com/",
   },
 ];
 
 const HeaderBar: React.FC = () => {
   return (
-    <div className="bg-charcoal p-3 text-white">
-      <div className="container mx-auto flex justify-end items-center space-x-4">
+    <div className="bg-charcoal text-white h-10">
+      <div className="container mx-auto px-5 max-w-84rem flex space-x-4 h-full">
         {socialLinks.map((link, index) => (
           <a
             key={index}
             href={link.hyperlink}
             target="_blank"
             rel="noopener noreferrer"
-            className="text-white hover:text-gray-400"
+            className="text-white hover:text-gray-400 flex-shrink-0 my-auto"
           >
             {link.icon}
           </a>
