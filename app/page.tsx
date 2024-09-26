9f198454b915ea9b9e9336c515d2878eb449d2f4
Dimetrius Hightower <dimetrius.hightower@gmail.com>
2024-09-26 16:52:07 -0400

    removing hero post title

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
 import { draftMode } from "next/headers";
-import Link from "next/link";
 
-import Date from "./components/date";
 import MoreStories from "./components/more-stories";
 import CoverImage from "./cover-image";
 
 import { getAllPosts } from "@/lib/api";
-import { Box } from "@chakra-ui/react";
-import { FaThumbtack } from "react-icons/fa";
 // import Intro from "./components/intro";
 import Intro from "./components/intro";
-import Label from "./components/label";
-import { truncateText } from "./util/text-util";
 
 const HERO_SLUG = "prologue";
 
@@ -38,31 +32,6 @@ function HeroPost({
           url={coverImage.url}
           borderRadius={"full"}
         />
-      </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-        <div>
-          <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
-            <Link href={`/posts/${slug}`} className="hover:underline">
-              {title}
-            </Link>
-          </h3>
-          <Box display="flex" gap={2}>
-            <Box display="flex" alignItems="center">
-              <FaThumbtack
-                color="#7A8450"
-                style={{ height: "100%", width: "auto" }}
-              />
-            </Box>
-            <Label>
-              <Date dateString={date} />
-            </Label>
-          </Box>
-        </div>
-        <div>
-          <p className="text-lg leading-relaxed mt-4 md:mt-0 mb-4">
-            {truncateText(excerpt)}
-          </p>
-        </div>
       </div>
     </section>
   );
