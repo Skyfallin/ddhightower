4e0652884e0c73044be51c3e13dd11d9240c5652
Dimetrius Hightower <dimetrius.hightower@gmail.com>
2024-10-10 19:27:07 -0400

    attempt to fix page underline

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Divider,
   Flex,
@@ -34,83 +35,86 @@ const NavBar: React.FC = () => {
   console.log(pathName);
 
   return (
-    <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      flexDirection={"column"}
-    >
+    <Box>
       <Flex
-        className="container mx-auto px-5 max-w-84rem h-full"
-        justifyContent={"space-evenly"}
-        padding="1rem"
+        as="nav"
+        align="center"
+        justify="space-between"
+        flexDirection={"column"}
       >
-        <Flex align="center">
-          <InputGroup width="200px">
-            <InputLeftElement pointerEvents="none">
-              <FaMagnifyingGlass color="#33302e" />
-            </InputLeftElement>
-            <Input
-              borderColor={"#33302e"}
-              placeholder="Search..."
-              rounded="full"
-              paddingLeft="2.5rem" // Add left padding to prevent text overlap
-            />
-          </InputGroup>
+        <Flex
+          className="container mx-auto px-5 max-w-84rem h-full"
+          justifyContent={"space-evenly"}
+          padding="1rem"
+        >
+          <Flex align="center">
+            <InputGroup width="200px">
+              <InputLeftElement pointerEvents="none">
+                <FaMagnifyingGlass color="#33302e" />
+              </InputLeftElement>
+              <Input
+                borderColor={"#33302e"}
+                placeholder="Search..."
+                rounded="full"
+                paddingLeft="2.5rem" // Add left padding to prevent text overlap
+              />
+            </InputGroup>
+          </Flex>
+
+          <Link
+            href="/"
+            _hover={{ textDecoration: "none" }} // Removes underline on hover
+          >
+            <Text
+              color={"#33302e"}
+              display={{ base: "none", md: "block" }} // Only show on larger screens
+              fontFamily="'Libre Baskerville', serif"
+              fontSize="6xl"
+              fontWeight="bold"
+            >
+              D. D. Hightower
+            </Text>
+          </Link>
+
+          <Button
+            className="bg-text-charcoal-light hover:bg-text-white hover:text-charcoal-light border border-charcoal text-white font-bold duration-200 transition-colors mb-6 lg:mb-0"
+            borderRadius="0"
+            backgroundColor="#33302e"
+            _hover={{ backgroundColor: "white" }}
+            my={"auto"}
+          >
+            Contact
+          </Button>
         </Flex>
 
-        <Link
-          href="/"
-          _hover={{ textDecoration: "none" }} // Removes underline on hover
-        >
-          <Text
-            color={"#33302e"}
-            display={{ base: "none", md: "block" }} // Only show on larger screens
-            fontFamily="'Libre Baskerville', serif"
-            fontSize="6xl"
-            fontWeight="bold"
-          >
-            D. D. Hightower
-          </Text>
-        </Link>
+        <Divider borderColor={"#33302e"} opacity={0.25} />
 
-        <Button
-          className="bg-text-charcoal-light hover:bg-text-white hover:text-charcoal-light border border-charcoal text-white font-bold duration-200 transition-colors mb-6 lg:mb-0"
-          borderRadius="0"
-          backgroundColor="#33302e"
-          _hover={{ backgroundColor: "white" }}
-          my={"auto"}
-        >
-          Contact
-        </Button>
+        <Flex fontWeight={"bold"} gap={4} py={2}>
+          {navLinks.map((link, index) => {
+            const isActive = pathName === link.hyperlink;
+
+            return (
+              <Link
+                key={index}
+                href={link.hyperlink}
+                rel="noopener noreferrer"
+                textColor={"#990f3d"}
+                _hover={{ color: "blue.600" }}
+                flexShrink={0}
+                textDecoration={isActive ? "underline" : "none"}
+                textUnderlineOffset="12px"
+                textDecorationThickness="2px"
+              >
+                {link.text}
+              </Link>
+            );
+          })}
+        </Flex>
+
+        {/* <Divider borderColor={"#33302e"} opacity={0.25} /> */}
       </Flex>
-
       <Divider borderColor={"#33302e"} opacity={0.25} />
-
-      <Flex fontWeight={"bold"} gap={4} py={2}>
-        {navLinks.map((link, index) => {
-          const isActive = pathName === link.hyperlink;
-
-          return (
-            <Link
-              key={index}
-              href={link.hyperlink}
-              rel="noopener noreferrer"
-              textColor={"#990f3d"}
-              _hover={{ color: "blue.600" }}
-              flexShrink={0}
-              textDecoration={isActive ? "underline" : "none"}
-              textUnderlineOffset="12px"
-              textDecorationThickness="2px"
-            >
-              {link.text}
-            </Link>
-          );
-        })}
-      </Flex>
-
-      <Divider borderColor={"#33302e"} opacity={0.25} />
-    </Flex>
+    </Box>
   );
 };
 
