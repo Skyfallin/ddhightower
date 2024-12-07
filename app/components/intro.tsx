2e8a95625b4a2c48dfbea39ec6897fe826f0c2c2
Dimetrius Hightower <dimetrius.hightower@gmail.com>
2024-12-07 18:52:23 -0500

    updating 'blog' and 'd.d. hightower' text heading styles

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
 
 const Intro = () => {
   return (
@@ -6,28 +6,21 @@ const Intro = () => {
       <Box
         flex={1.5}
         alignSelf="center"
-        display={{ base: "block", md: "none" }} // Only show on small screens
+        display={{ base: 'block', md: 'none' }} // Only show on small screens
       >
-        <Heading
-          as="h1"
-          color={"#33302e"}
-          fontFamily="'Libre Baskerville', serif"
-          fontSize="4xl"
-          fontWeight="bold"
-          letterSpacing="-1px"
-        >
+        <Heading as="h1" fontSize="4xl" fontWeight="bold" letterSpacing="-1px">
           D. D. Hightower
         </Heading>
       </Box>
-      <Box alignContent={"center"} flex={1} padding={4}>
-        <Text fontSize={["sm", "sm", "md"]} textAlign={"center"}>
+      <Box alignContent={'center'} flex={1} padding={4}>
+        <Text fontSize={['sm', 'sm', 'md']} textAlign={'center'}>
           Explore the creative world of D. D. Hightower, a New York-based
           fantasy novelist. Dive into a collection of exclusive preview chapters
           and other exciting writing projects.
         </Text>
       </Box>
     </Flex>
-  );
-};
+  )
+}
 
-export default Intro;
+export default Intro
