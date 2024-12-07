8b4ade1938f37545dab2c6397119b9f83260616a
Dimetrius Hightower <dimetrius.hightower@gmail.com>
2024-12-07 18:21:25 -0500

    adding prettierrc

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -1,41 +1,41 @@
 // IMPORTANT: do not remove --------------------------
-"use client"; // react-slick uses client components
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
+'use client' // react-slick uses client components
+import 'slick-carousel/slick/slick-theme.css'
+import 'slick-carousel/slick/slick.css'
 // ---------------------------------------------------
 
-import { Box, Flex, Spinner, useMediaQuery } from "@chakra-ui/react";
-import React from "react";
-import Slider, { Settings } from "react-slick";
-import { BlogPostData } from "../types/blog-post";
-import BlogPost from "./blog-post";
+import { Box, Flex, Spinner, useMediaQuery } from '@chakra-ui/react'
+import React from 'react'
+import Slider, { Settings } from 'react-slick'
+import { BlogPostData } from '../types/blog-post'
+import BlogPost from './blog-post'
 
 type CarouselProps = Readonly<{
   /**
    * Data of entries from Contentful collection
    */
-  data: BlogPostData[];
+  data: BlogPostData[]
   /**
    * Whether to display pagination dots.
    */
-  dots: boolean;
+  dots: boolean
   /**
    * Whether the carousel should infinitely loop.
    */
-  infinite: boolean;
+  infinite: boolean
   /**
    * The speed of the animation.
    */
-  speed: number;
+  speed: number
   /**
    * How many slides to display per page.
    */
-  slidesToShow: number;
+  slidesToShow: number
   /**
    * How many slides to scroll per page.
    */
-  slidesToScroll: number;
-}>;
+  slidesToScroll: number
+}>
 
 const Carousel: React.FC<CarouselProps> = ({
   data,
@@ -45,11 +45,11 @@ const Carousel: React.FC<CarouselProps> = ({
   slidesToShow,
   slidesToScroll,
 }) => {
-  const [isMobile] = useMediaQuery("(max-width: 767px)");
+  const [isMobile] = useMediaQuery('(max-width: 767px)')
 
   const settings: Settings = {
     dots,
-    dotsClass: "slick-dots slick-thumb",
+    dotsClass: 'slick-dots slick-thumb',
     infinite,
     speed,
     slidesToShow: isMobile ? 1 : slidesToShow,
@@ -58,25 +58,25 @@ const Carousel: React.FC<CarouselProps> = ({
       <div
         key={i}
         style={{
-          borderRadius: "50%",
-          fontSize: "14px",
-          padding: "5px",
-          cursor: "pointer",
-          fontWeight: "bold",
-          width: "30px", 
+          borderRadius: '50%',
+          fontSize: '14px',
+          padding: '5px',
+          cursor: 'pointer',
+          fontWeight: 'bold',
+          width: '30px',
         }}
       >
         {i + 1}
       </div>
     ),
-  };
+  }
 
   if (!data) {
-  return (
-    <Flex justifyContent={'center'}>
-      <Spinner />
+    return (
+      <Flex justifyContent={'center'}>
+        <Spinner />
       </Flex>
-  )   
+    )
   }
 
   return (
@@ -84,17 +84,17 @@ const Carousel: React.FC<CarouselProps> = ({
       <Slider {...settings}>
         {data.map((post, index: number) => (
           <BlogPost
-            imageSrc={post.coverImage?.url ?? ""}
-            date={post.date ?? ""}
-            title={post.title ?? ""}
-            excerpt={post.excerpt ?? ""}
-            slug={post.slug ?? ""}
+            imageSrc={post.coverImage?.url ?? ''}
+            date={post.date ?? ''}
+            title={post.title ?? ''}
+            excerpt={post.excerpt ?? ''}
+            slug={post.slug ?? ''}
             key={index}
           />
         ))}
       </Slider>
     </Box>
-  );
-};
+  )
+}
 
-export default Carousel;
+export default Carousel
