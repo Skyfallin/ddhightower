2467980ba19d2c2bab35abf1e9c2862d8191f82f
Dimetrius Hightower <dimetrius.hightower@gmail.com>
2024-12-07 19:58:55 -0500

    fix footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,30 +1,20 @@
-import { Divider, Flex, Link, Text } from "@chakra-ui/react";
+import { Divider, Flex, Text } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
     <Flex
-      alignItems={"center"}
-      //   bg="green"
+      alignItems={'center'}
       color="gray.500"
-      flexDirection={"column"}
+      flexDirection={'column'}
       gap={8}
       mt={10}
       py={10}
-      justifyContent={"center"}
+      justifyContent={'center'}
     >
-      <Divider borderColor={"#33302e"} opacity={0.25} />
-      <Text fontSize="sm">
-        © 2024 D. D. Hightower. All Rights Reserved |{" "}
-        <Link href="/privacy" color="teal.400">
-          Privacy Policy
-        </Link>{" "}
-        |{" "}
-        <Link href="/terms" color="teal.400">
-          Terms of Service
-        </Link>
-      </Text>
+      <Divider borderColor={'#33302e'} opacity={0.25} />
+      <Text fontSize="sm">© 2024 D. D. Hightower. All Rights Reserved</Text>
     </Flex>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
