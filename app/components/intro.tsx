4d9359166fb8f3d78d563c9c8107541103d6ebf6
Dimetrius Hightower <dimetrius.hightower@gmail.com>
2024-12-13 12:53:25 -0500

    cleaning up mobile spacing

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -2,13 +2,13 @@ import { Box, Flex, Heading, Text } from '@chakra-ui/react'
 
 const Intro = () => {
   return (
-    <Flex flexDirection="column" paddingY={4}>
-      <Box flex={1.5} alignSelf="center" hideFrom={'md'}>
+    <Flex flexDirection="column" gap={4} padding={4}>
+      <Box alignSelf="center" hideFrom={'md'}>
         <Heading as="h1" fontSize="4xl" fontWeight="bold" letterSpacing="-1px">
           D. D. Hightower
         </Heading>
       </Box>
-      <Box alignContent={'center'} flex={1} padding={4}>
+      <Box alignContent={'center'} paddingY={{ base: '0', md: '4' }}>
         <Text fontSize={['sm', 'sm', 'md']} textAlign={'center'}>
           Explore the creative world of D. D. Hightower, a New York-based
           fantasy novelist. Dive into a collection of exclusive preview chapters
